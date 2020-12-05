8cd6dffe65694f8e9f2b017a9d20ccfc49c76c84
Tenzin <tenzin606gaphel@gmail.com>
2020-12-05 22:51:26 +0600

    box component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from "./components/box/box.component";
+import SceneComponent from "./components/scene/scene.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/*<Box/>*/}
+            <SceneComponent/>
+        </div>
+    );
 }
 
 export default App;
